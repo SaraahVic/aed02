d6a73188650e8e6e9a18e686942a7f68ca929cc8
SaraahVic <88517482+SaraahVic@users.noreply.github.com>
2023-04-14 23:03:42 -0300

    Delete questao07.h

--- questao07.h
+++
@@ -1,9 +0,0 @@
-void questao07(void)
-{
-    float radius, pi = 3.14;
-    printf("digite o raio do circulo: ");
-    scanf("%f", &radius);
-    printf("perimetro: %f\n", 2*pi*radius);
-    printf("area: %f\n", pi*radius*radius);
-    printf("diametro: %f\n", 2*radius);
-}
