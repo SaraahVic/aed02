b038400143d781d2aa88eb9d337d7d1c7689e0df
SaraahVic <88517482+SaraahVic@users.noreply.github.com>
2023-04-14 23:04:22 -0300

    Delete questao13.h

--- questao13.h
+++
@@ -1,8 +0,0 @@
-void questao13(void)
-{
-    int num;
-    printf("digite um numero: ");
-    scanf("%d", &num);
-    if (num%5) { printf("numero nao e divisivel por 5"); }
-    else { printf("numero e divisivel por 5"); }
-}
