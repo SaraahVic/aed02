259ee82ee427a8813676fdc990b4ed1c431610ba
SaraahVic <88517482+SaraahVic@users.noreply.github.com>
2023-04-14 23:03:35 -0300

    Delete questao06.h

--- questao06.h
+++
@@ -1,9 +0,0 @@
-void questao06(void)
-{
-    float old, new;
-    printf("digite o valor antigo: ");
-    scanf("%f", &old);
-    printf("digite o valor novo: ");
-    scanf("%f", &new);
-    printf("porcentagem de aumento: %.2f%%", (new-old)/old*100);
-}
