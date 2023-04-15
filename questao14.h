61096bc7fd0cc47e5893533e79068efd3848b0b6
SaraahVic <88517482+SaraahVic@users.noreply.github.com>
2023-04-14 23:05:41 -0300

    Delete questao14.h

--- questao14.h
+++
@@ -1,8 +0,0 @@
-void questao14(void)
-{
-    int num;
-    printf("digite um numero: ");
-    scanf("%d", &num);
-    if (num%2) { printf("numero e impar"); }
-    else { printf("numero e par"); }
-}
