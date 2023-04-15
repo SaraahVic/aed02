b602a6fca8843d5d9a10a0400009f44c31c1455e
SaraahVic <88517482+SaraahVic@users.noreply.github.com>
2023-04-14 23:03:49 -0300

    Delete questao08.h

--- questao08.h
+++
@@ -1,7 +0,0 @@
-void questao08(void)
-{
-    float vel;
-    printf("digite a velocidade do veiculo (Km/h): ");
-    scanf("%f", &vel);
-    printf("velocidade em metros por segundo: %f", vel/3.6);
-}
