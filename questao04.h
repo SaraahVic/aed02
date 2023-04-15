0f5ee3cf38ae6d44ac1802bcf5c6b23f28fff0ed
SaraahVic <88517482+SaraahVic@users.noreply.github.com>
2023-04-14 23:03:22 -0300

    Delete questao04.h

--- questao04.h
+++
@@ -1,9 +0,0 @@
-void questao04(void)
-{
-    float salary, raise_percentage;
-    printf("digite o salario atual: ");
-    scanf("%f", &salary);
-    printf("digite a porcentagem de aumento: ");
-    scanf("%f", &raise_percentage);
-    printf("salario apos aumento: %f", (salary*raise_percentage)/100);
-}
